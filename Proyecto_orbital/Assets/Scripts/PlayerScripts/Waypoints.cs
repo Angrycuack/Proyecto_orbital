7c28c21ce0cfac655e7926ffbdbaf8af3137fa23
devmichael <dev.micsaltat@gmail.com>
2021-07-22 22:43:40 +0200

    hey estado experimentando con el waypoint o sea la linea que sigue el jugador. Nuevos scripts añadidos Waypoints y PlayerMovement.

---
+++ Proyecto_orbital/Assets/Scripts/PlayerScripts/Waypoints.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public Color rayColor = Color.black;
+    public List<Transform> wayPoints = new List<Transform>();
+    Transform[] theArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = rayColor;
+        theArray = GetComponentsInChildren<Transform>();
+        wayPoints.Clear();
+
+        foreach (Transform wayPoint in theArray)
+        {
+            if (wayPoint != this.transform)
+            {
+                wayPoints.Add(wayPoint);
+            }
+        }
+
+        for (int i = 0; i < wayPoints.Count; i++)
+        {
+            Vector3 position = wayPoints[i].position;
+            if (i > 0)
+            {
+                Vector3 previous = wayPoints[i - 1].position;
+                Gizmos.DrawLine(previous, position);
+            }
+        }
+    }
+}
