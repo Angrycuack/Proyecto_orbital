e08a9e9f2f73aa941020e3d8e117c1f1b5f79390
devmichael <dev.micsaltat@gmail.com>
2021-07-30 10:47:07 +0200

    Implementado un loop en el scripts powerupSpawner para que aparezcan la cantidad de power ups en la postion en el eje Z aleatoriamente

--- Proyecto_orbital/Assets/Scripts/Generator/PowerUpSpawner.cs
+++ Proyecto_orbital/Assets/Scripts/Generator/PowerUpSpawner.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class PowerUpSpawner : MonoBehaviour
 {
-    [SerializeField] public GameObject[] powerUps;
-    [SerializeField] public int numberOfPowerUps;
+    [Header ("Asignar prefabs de los powerUps y cantidad")]
+    [SerializeField] public GameObject DestructionPU;
+    [SerializeField] public int sizeDestructionPU;
+    GameObject[] poolDestructionPU; // grupacion
+    [SerializeField] public GameObject FrostPU;
+    [SerializeField] public int sizeFrostPU;
+    GameObject[] poolFrostPU; // grupacion
+    [SerializeField] public GameObject GhostPU;
+    [SerializeField] public int sizeGhostPU;
+    GameObject[] poolGhostPU; // grupacion
+    [SerializeField] public GameObject MultiplePU;
+    [SerializeField] public int sizeMultiplePU;
+    GameObject[] poolMultiplePU; // grupacion
+    [SerializeField] public GameObject NearPU;
+    [SerializeField] public int sizeNearPU;
+    GameObject[] poolNearPU; // grupacion
+    [SerializeField] public GameObject SpeedPU;
+    [SerializeField] public int sizeSpeedPU;
+    GameObject[] poolSpeedPU; // grupacion
 
+    
+
+
+    [Header ("Asignar el Jugador y el target position final")]
     public GameObject player;
     Vector3 playerPosition;
 
@@ -16,16 +37,20 @@ public class PowerUpSpawner : MonoBehaviour
     //private bool go = true;
     void Awake()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.Find("Player 1");
         playerPosition = player.transform.position;
-        target = GameObject.Find("TargetPosition");
+        target = GameObject.Find("Final WayPoints");
         targetEndPosition = target.transform.position;
+        PowerUpsRecharger();
     }
+
+
+
     void Start()
     {
         Debug.Log("Posicion del jugador" + playerPosition);
         Debug.Log("Posicion target " + targetEndPosition);
-        PointsGeneratorPowerUps(numberOfPowerUps);
+        
     }
 
     // Update is called once per frame
@@ -33,12 +58,28 @@ public class PowerUpSpawner : MonoBehaviour
     {
         
     }
+
     /// <summary>
-    /// Generar los puntos de apareceran los power ups aleatoriamente
+    /// Se inicializa con el numero de powerUps asignado.
     /// </summary>
-    /// <param name="numberOfpowerups"></param>
-    void PointsGeneratorPowerUps (int numberOfpowerups)
+    void PowerUpsRecharger()
     {
-        
+        poolDestructionPU = new GameObject[sizeDestructionPU];
+
+        for(int i = 0; i < poolDestructionPU.Length; i++)
+        {
+            float posZ = targetEndPosition.z;
+            
+            float randomPosition = Random.Range(0, posZ);
+            if(i < sizeDestructionPU)
+            {
+                poolDestructionPU[i] = Instantiate(DestructionPU, new Vector3(0, 0, randomPosition), Quaternion.identity);
+                poolDestructionPU[i].SetActive(true);
+            }
+            
+            
+        }
+
     }
+
 }
