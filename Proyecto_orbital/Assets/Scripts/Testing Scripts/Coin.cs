e286c7d96d299a24c4dbe63e60c7ee2ec5243c2b
devmichael <dev.micsaltat@gmail.com>
2021-08-05 00:57:56 +0200

    Arreglado el error que tenía cambiando el valor max del random.range

--- Proyecto_orbital/Assets/Scripts/Testing Scripts/Coin.cs
+++ Proyecto_orbital/Assets/Scripts/Testing Scripts/Coin.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public static int playerCoins = 0;
-    public int[] valueCoinOne = new int[5] {1, 5, 10, 15, 20};
+    public int[] valueCoins = {1,5,15};
     public void PickedPlayerCoin()
     {
-        int coinNum = Random.Range(0, valueCoinOne.Length+1);
-        playerCoins  =+ valueCoinOne[coinNum];
+        int coinNum = Random.Range(0, 2);
+        playerCoins = playerCoins + valueCoins[coinNum];
         Debug.Log("El jugador ha conseguido " + playerCoins + " monedas");
-        Debug.Log("El numero es " + coinNum + " monedas");
+        Debug.Log("El numero es " + valueCoins + " monedas");
+        
+      
+           
     }
 
 }
