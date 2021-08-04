be1bd3eb9f7b829e8953175961b86f358fd20ad4
devmichael <dev.micsaltat@gmail.com>
2021-08-04 23:27:19 +0200

    Error en la linea 12

--- Proyecto_orbital/Assets/Scripts/Testing Scripts/Coin.cs
+++ Proyecto_orbital/Assets/Scripts/Testing Scripts/Coin.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public static int playerCoins = 0;
-    [SerializeField]
-    public int[] valueCoinOne;
+    public int[] valueCoinOne = new int[5] {1, 5, 10, 15, 20};
     public void PickedPlayerCoin()
     {
-        int coinNum = Random.Range(0, valueCoinOne.Length);
-        playerCoins += valueCoinOne[coinNum];
+        int coinNum = Random.Range(0, valueCoinOne.Length+1);
+        playerCoins  =+ valueCoinOne[coinNum];
         Debug.Log("El jugador ha conseguido " + playerCoins + " monedas");
         Debug.Log("El numero es " + coinNum + " monedas");
     }
